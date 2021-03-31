014b4f48743d6410d2912ac473489cfc04101150
bujji-swetha <81677483+bujji-swetha@users.noreply.github.com>
2021-03-31 13:42:36 +0530

    Add files via upload

---
+++ perimeter.java
@@ -0,0 +1,12 @@
+class perimeter
+{
+public static void main(String a[])
+{
+int radius=10;
+final float PI=3.14f;
+float area=PI*radius*radius;
+float perimeter=2*PI*radius;
+System.out.println("Area of circle="+area);
+System.out.println("Perimeter of circle="+perimeter);
+}
+}
